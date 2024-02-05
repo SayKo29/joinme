f37a1a5b0099e849667d10fb485df1ff598c3c8f
SayKo29 <aaronsar29@gmail.com>
2024-02-05 13:38:35 +0100

    some tests

---
+++ cypress/e2e/api_petition.cy.ts
@@ -0,0 +1,19 @@
+// This tells VS Code to autocomplete cypress
+/// <reference types="cypress" />
+describe("Validate events api call", () => {
+    it("Validate events api call", () => {
+        cy.request({
+            method: "GET",
+            url: "https://calm-lime-armadillo.cyclic.app/api/events",
+        }).then((response) => {
+            expect(response.status).to.eq(200);
+            expect(response.body).to.not.be.null;
+            expect(response.body[0]).to.have.property("_id");
+            expect(response.body[0]).to.have.property("name");
+            expect(response.body[0]).to.have.property("description");
+            expect(response.body[0]).to.have.property("startDate");
+            expect(response.body[0]).to.have.property("endDate");
+            expect(response.body[0]).to.have.property("location");
+        });
+    });
+});
