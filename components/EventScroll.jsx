cd10f004943c31aaebf7bdf5dd011750d716b4c7
SayKo29 <aaronsar29@gmail.com>
2024-05-13 12:11:18 +0200

    pretty

--- components/EventScroll.jsx
+++ components/EventScroll.jsx
@@ -1,118 +1,121 @@
-import { Platform, StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
-import colors from '@/styles/colors'
-import EventCard from './EventCard'
-import { FlashList } from '@shopify/flash-list'
-import { useAuth } from '@/contexts/Auth'
-import { useNavigation } from '@react-navigation/native'
-import useEventStore from 'store/EventStore'
-import useUsersStore from 'store/UsersStore'
-import CustomBottomTab from './ui/CustomBottomTab'
-import HeaderNavigationEvent from './HeaderNavigationEvent'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { sortEventsByCloser } from 'lib/utils'
+import { Platform, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import colors from "@/styles/colors";
+import EventCard from "./EventCard";
+import { FlashList } from "@shopify/flash-list";
+import { useAuth } from "@/contexts/Auth";
+import { useNavigation } from "@react-navigation/native";
+import useEventStore from "store/EventStore";
+import useUsersStore from "store/UsersStore";
+import CustomBottomTab from "./ui/CustomBottomTab";
+import HeaderNavigationEvent from "./HeaderNavigationEvent";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { sortEventsByCloser } from "lib/utils";
 
 const EventScroll = () => {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    const handleEventPress = (event, user) => {
-        navigation.navigate('EventDetailScreen', { event, user })
+  const handleEventPress = (event, user) => {
+    navigation.navigate("EventDetailScreen", { event, user });
+  };
+  const data = useEventStore((state) => state.events);
+  const users = useUsersStore((state) => state.users);
+  const auth = useAuth();
+  const userLogged = auth?.authData.user;
+
+  const events = data
+    ? data.filter((event) => event.user !== userLogged._id)
+    : [];
+  const [eventsSorted, setEventsSorted] = useState([]);
+  // sort events by distance
+  // get user location from async storage
+  const getUserLocation = async () => {
+    try {
+      const userLocation = await AsyncStorage.getItem("region");
+      return JSON.parse(userLocation);
+    } catch (error) {
+      console.log(error);
     }
-    const data = useEventStore((state) => state.events)
-    const users = useUsersStore((state) => state.users)
-    const auth = useAuth()
-    const userLogged = auth?.authData.user
+  };
 
-    const events = data
-        ? data.filter((event) => event.user !== userLogged._id)
-        : []
-    const [eventsSorted, setEventsSorted] = useState([])
-    // sort events by distance
-    // get user location from async storage
-    const getUserLocation = async () => {
-        try {
-            const userLocation = await AsyncStorage.getItem('region');
-            return JSON.parse(userLocation);
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    const sortEventsByDistance = async () => {
-        try {
-            const userLocation = await getUserLocation();
-            const { latitude, longitude } = userLocation;
-            const eventsSorted = sortEventsByCloser(latitude, longitude, events);
-            setEventsSorted(eventsSorted);
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    React.useEffect(effect => {
-        sortEventsByDistance();
-    }, []);
-
-
-    const user = users
-    if (eventsSorted.length === 0) {
-        return (
-            <View style={styles.container}>
-                <View style={styles.headerContainer}>
-                    <HeaderNavigationEvent />
-                </View>
-                <View style={styles.center}>
-                    <Text style={styles.noEvents}>No hay eventos disponibles</Text>
-                </View>
-                <View style={styles.bottomTab}>
-                    <CustomBottomTab />
-                </View>
-            </View>
-        )
+  const sortEventsByDistance = async () => {
+    try {
+      const userLocation = await getUserLocation();
+      const { latitude, longitude } = userLocation;
+      const eventsSorted = sortEventsByCloser(latitude, longitude, events);
+      setEventsSorted(eventsSorted);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
+  React.useEffect((effect) => {
+    sortEventsByDistance();
+  }, []);
+
+  const user = users;
+  if (eventsSorted.length === 0) {
     return (
-        <View style={[styles.container]}>
-            <View style={styles.headerContainer}>
-                <HeaderNavigationEvent />
-            </View>
-            <FlashList
-                data={eventsSorted}
-                renderItem={({ item, index }) => (
-                    <EventCard event={item} user={user} onEventPress={handleEventPress} index={index} />
-                )}
-                estimatedItemSize={20}
-            />
-            <View style={styles.bottomTab}>
-                <CustomBottomTab />
-            </View>
-        </View >
-    )
-}
+      <View style={styles.container}>
+        <View style={styles.headerContainer}>
+          <HeaderNavigationEvent />
+        </View>
+        <View style={styles.center}>
+          <Text style={styles.noEvents}>No hay eventos disponibles</Text>
+        </View>
+        <View style={styles.bottomTab}>
+          <CustomBottomTab />
+        </View>
+      </View>
+    );
+  }
+
+  return (
+    <View style={[styles.container]}>
+      <View style={styles.headerContainer}>
+        <HeaderNavigationEvent />
+      </View>
+      <FlashList
+        data={eventsSorted}
+        renderItem={({ item, index }) => (
+          <EventCard
+            event={item}
+            user={user}
+            onEventPress={handleEventPress}
+            index={index}
+          />
+        )}
+        estimatedItemSize={20}
+      />
+      <View style={styles.bottomTab}>
+        <CustomBottomTab />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: colors.background,
-        paddingTop: Platform.OS === 'android' ? 0 : 55,
-        width: '100%',
-        height: '100%',
-        flex: 1
-    },
-    headerContainer: {
-        height: 30
-    },
-    center: {
-        backgroundColor: colors.background,
-        flex: 1,
-        justifyContent: 'center',
-    },
-    noEvents: {
-        color: colors.text,
-        fontSize: 20,
-        fontWeight: 'bold',
-        textAlign: 'center'
-    },
+  container: {
+    backgroundColor: colors.background,
+    paddingTop: Platform.OS === "android" ? 0 : 55,
+    width: "100%",
+    height: "100%",
+    flex: 1,
+  },
+  headerContainer: {
+    height: 30,
+  },
+  center: {
+    backgroundColor: colors.background,
+    flex: 1,
+    justifyContent: "center",
+  },
+  noEvents: {
+    color: colors.text,
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
 
-})
-
-export default EventScroll
+export default EventScroll;
--- screens/CreateEvent.jsx
+++ screens/CreateEvent.jsx
@@ -1,287 +1,285 @@
-import {
-    Platform,
-    StyleSheet,
-    Text,
-    View
-} from 'react-native'
-import React, { useEffect } from 'react'
-import SelectCategory from '@/components/CreateEvent/SelectCategory'
-import colors from '@/styles/colors'
-import EventInfo from '@/components/CreateEvent/EventInfo'
-import { ProgressSteps, ProgressStep } from 'react-native-progress-steps'
-import AdvancedEventInfo from '@/components/CreateEvent/AdvancedEventInfo'
-import CreateEventPost from '@/api/CreateEventPost'
-import { useAuth } from '@/contexts/Auth'
-import LottieAnimation from '@/components/LottieAnimation'
-import CustomBottomTab from 'components/ui/CustomBottomTab'
-import useHeaderEventStore from 'store/HeaderEventStore'
-import useTabStore from 'store/TabStore'
-import { useNavigation } from '@react-navigation/native'
-import Toast from 'react-native-toast-message'
-import { useQueryClient } from 'react-query'
-import EditEvent from 'api/EditEvent'
+import { Platform, StyleSheet, Text, View } from "react-native";
+import React, { useEffect } from "react";
+import SelectCategory from "@/components/CreateEvent/SelectCategory";
+import colors from "@/styles/colors";
+import EventInfo from "@/components/CreateEvent/EventInfo";
+import { ProgressSteps, ProgressStep } from "react-native-progress-steps";
+import AdvancedEventInfo from "@/components/CreateEvent/AdvancedEventInfo";
+import CreateEventPost from "@/api/CreateEventPost";
+import { useAuth } from "@/contexts/Auth";
+import LottieAnimation from "@/components/LottieAnimation";
+import CustomBottomTab from "components/ui/CustomBottomTab";
+import useHeaderEventStore from "store/HeaderEventStore";
+import useTabStore from "store/TabStore";
+import { useNavigation } from "@react-navigation/native";
+import Toast from "react-native-toast-message";
+import { useQueryClient } from "react-query";
+import EditEvent from "api/EditEvent";
+
 const CreateEvent = ({ route }) => {
-    const navigation = useNavigation()
-    const queryClient = useQueryClient()
-    const [category, setCategory] = React.useState('')
-    const [loading, setLoading] = React.useState(false)
-    const [isEditing, setIsEditing] = React.useState(false)
-    const [event, setEvent] = React.useState({
-        name: '',
-        description: '',
-        category: '',
-        location: '',
+  const navigation = useNavigation();
+  const queryClient = useQueryClient();
+  const [category, setCategory] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
+  const [isEditing, setIsEditing] = React.useState(false);
+  const [event, setEvent] = React.useState({
+    name: "",
+    description: "",
+    category: "",
+    location: "",
+    images: {},
+    isRemote: false,
+    user: "",
+    startDate: "",
+    endDate: "",
+    participants: [],
+    chatroom: "",
+  });
+
+  useEffect(() => {
+    if (route.params?.eventEdit) {
+      setEvent(route.params.eventEdit);
+      setCategory(route.params.eventEdit.category);
+      setIsEditing(true);
+    }
+  }, [route.params]);
+
+  const auth = useAuth();
+  const user = auth?.authData?.user;
+
+  // updateEvent changes the event state with the key and value passed
+  const updateEvent = (key, value) => {
+    setEvent((oldEvent) => ({
+      ...oldEvent,
+      [key]: value,
+    }));
+  };
+
+  const handleEventCreation = async () => {
+    await createEvent();
+  };
+
+  const editEvent = async () => {
+    const eventToSend = {
+      ...event,
+      category,
+      startDate: new Date(event.startDate).toISOString(),
+      endDate: new Date(event.endDate).toISOString(),
+      user: user._id,
+    };
+
+    try {
+      await EditEvent(eventToSend);
+      // invalidate the query to get the new data
+      queryClient.invalidateQueries("events");
+      setEvent({
+        /* estado inicial del evento */
+      });
+      useHeaderEventStore.setState({ tab: "MyEvents" });
+      useTabStore.setState({ tab: 0 });
+      navigation.navigate("MyEvents");
+      Toast.show({
+        position: "bottom",
+        type: "success",
+        text1: "Evento editado correctamente",
+        visibilityTime: 3000,
+      });
+    } catch (error) {
+      console.error("Error al editar el evento:", error);
+      Toast.show({
+        type: "error",
+        text1: "Error al editar el evento",
+        text2: "Por favor, inténtalo de nuevo más tarde",
+        visibilityTime: 3000,
+      });
+    }
+  };
+
+  const createEvent = async () => {
+    setLoading(true);
+
+    const eventToSend = {
+      ...event,
+      category,
+      startDate: new Date(event.startDate).toISOString(),
+      endDate: new Date(event.endDate).toISOString(),
+      user: user._id,
+    };
+
+    try {
+      await CreateEventPost(eventToSend);
+      // invalidate the query to get the new data
+      queryClient.invalidateQueries("events");
+      setEvent({
+        /* estado inicial del evento */
+      });
+      useHeaderEventStore.setState({ tab: "MyEvents" });
+      useTabStore.setState({ tab: 0 });
+      navigation.navigate("MyEvents");
+      Toast.show({
+        position: "bottom",
+        type: "success",
+        text1: "Evento creado correctamente",
+        visibilityTime: 3000,
+      });
+    } catch (error) {
+      console.error("Error al crear el evento:", error);
+      Toast.show({
+        type: "error",
+        text1: "Error al crear el evento",
+        text2: "Por favor, inténtalo de nuevo más tarde",
+        visibilityTime: 3000,
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  React.useEffect(() => {
+    const resetEventState = () => {
+      setEvent({
+        name: "",
+        description: "",
+        category: "",
+        location: "",
         images: {},
-        isRemote: false,
-        user: '',
-        startDate: '',
-        endDate: '',
+        user: "",
+        startDate: "",
+        endDate: "",
         participants: [],
-        chatroom: ''
-    })
-
-    useEffect(() => {
-        if (route.params?.eventEdit) {
-            setEvent(route.params.eventEdit)
-            setCategory(route.params.eventEdit.category)
-            setIsEditing(true)
-        }
-    }, [route.params])
-
-
-    const auth = useAuth()
-    const user = auth?.authData?.user
-
-    // updateEvent changes the event state with the key and value passed
-    const updateEvent = (key, value) => {
-        setEvent((oldEvent) => ({
-            ...oldEvent,
-            [key]: value
-        }))
-    }
-
-    const handleEventCreation = async () => {
-        await createEvent();
+        chatroom: "",
+      });
     };
 
-    const editEvent = async () => {
-        const eventToSend = {
-            ...event,
-            category,
-            startDate: new Date(event.startDate).toISOString(),
-            endDate: new Date(event.endDate).toISOString(),
-            user: user._id
-        };
+    const unsubscribe = navigation.addListener("beforeRemove", resetEventState);
 
-        try {
-            await EditEvent(eventToSend);
-            // invalidate the query to get the new data
-            queryClient.invalidateQueries('events');
-            setEvent({/* estado inicial del evento */ });
-            useHeaderEventStore.setState({ tab: 'MyEvents' });
-            useTabStore.setState({ tab: 0 });
-            navigation.navigate('MyEvents');
-            Toast.show({
-                position: 'bottom',
-                type: 'success',
-                text1: 'Evento editado correctamente',
-                visibilityTime: 3000,
-            });
-        }
-        catch (error) {
-            console.error('Error al editar el evento:', error);
-            Toast.show({
-                type: 'error',
-                text1: 'Error al editar el evento',
-                text2: 'Por favor, inténtalo de nuevo más tarde',
-                visibilityTime: 3000,
-            });
-        }
-    }
-
-    const createEvent = async () => {
-        setLoading(true);
-
-        const eventToSend = {
-            ...event,
-            category,
-            startDate: new Date(event.startDate).toISOString(),
-            endDate: new Date(event.endDate).toISOString(),
-            user: user._id
-        };
-
-        try {
-            await CreateEventPost(eventToSend);
-            // invalidate the query to get the new data
-            queryClient.invalidateQueries('events');
-            setEvent({/* estado inicial del evento */ });
-            useHeaderEventStore.setState({ tab: 'MyEvents' });
-            useTabStore.setState({ tab: 0 });
-            navigation.navigate('MyEvents');
-            Toast.show({
-                position: 'bottom',
-                type: 'success',
-                text1: 'Evento creado correctamente',
-                visibilityTime: 3000,
-            });
-        } catch (error) {
-            console.error('Error al crear el evento:', error);
-            Toast.show({
-                type: 'error',
-                text1: 'Error al crear el evento',
-                text2: 'Por favor, inténtalo de nuevo más tarde',
-                visibilityTime: 3000,
-            });
-        } finally {
-            setLoading(false);
-        }
+    return () => {
+      unsubscribe();
+      resetEventState(); // Limpia el estado cuando se desmonta el componente
     };
+  }, [navigation]);
 
-
-    React.useEffect(() => {
-        const resetEventState = () => {
-            setEvent({
-                name: '',
-                description: '',
-                category: '',
-                location: '',
-                images: {},
-                user: '',
-                startDate: '',
-                endDate: '',
-                participants: [],
-                chatroom: ''
-            });
-        };
-
-        const unsubscribe = navigation.addListener('beforeRemove', resetEventState);
-
-        return () => {
-            unsubscribe();
-            resetEventState(); // Limpia el estado cuando se desmonta el componente
-        };
-    }, [navigation]);
-
-
-    return (
-        <View style={[styles.container]}>
-            <View style={styles.titleContainer}>
-                <Text style={styles.title}>{isEditing ? 'Editar evento' : 'Crear evento'}</Text>
-            </View>
-            {loading ? (
-                <View style={styles.loadingContainer}>
-                    <LottieAnimation />
-                </View>
-            ) : (
-                <View style={styles.stepWrapper}>
-                    <ProgressSteps
-                        activeStepNumColor={colors.text}
-                        completedProgressBarColor={colors.primary}
-                        activeLabelColor={colors.text}
-                        completedStepIconColor={colors.primary}
-                        completedCheckColor={colors.primary}
-                        activeStepIconBorderColor={colors.primary}
-                        activeStepIconColor={colors.primary}
-                        disabledStepIconColor={colors.disabled}
-                        disabledStepNumColor={colors.white}
-                        progressBarColor={colors.disabled}
-                        labelFontFamily='SignikaRegular'
-                    >
-                        <ProgressStep
-                            label='Categoría del evento'
-                            nextBtnStyle={styles.nextBtnStyle}
-                            nextBtnTextStyle={styles.nextBtnTextStyle}
-                            nextBtnText='Siguiente'
-                            nextBtnDisabled={category === ''}
-                            scrollable={false}
-                            viewProps={{ style: { flex: 1 } }}
-                        >
-                            <SelectCategory
-                                categorySelected={setCategory}
-                                activeCategory={category}
-                            />
-                        </ProgressStep>
-                        <ProgressStep
-                            label='Información básica'
-                            nextBtnStyle={styles.nextBtnStyle}
-                            nextBtnTextStyle={styles.nextBtnTextStyle}
-                            nextBtnText='Siguiente'
-                            previousBtnStyle={styles.previousBtnStyle}
-                            previousBtnTextStyle={styles.previousBtnTextStyle}
-                            previousBtnText='Atrás'
-                            nextBtnDisabled={event.name === '' || event.description === ''}
-                        >
-                            <EventInfo eventInfo={updateEvent} currentEvent={event} />
-                        </ProgressStep>
-                        <ProgressStep
-                            label='Información avanzada'
-                            nextBtnStyle={styles.nextBtnStyle}
-                            nextBtnTextStyle={styles.nextBtnTextStyle}
-                            previousBtnStyle={styles.previousBtnStyle}
-                            previousBtnTextStyle={styles.previousBtnTextStyle}
-                            previousBtnText='Atrás'
-                            finishBtnText={isEditing ? 'Guardar Cambios' : 'Crear Evento'}
-                            onSubmit={isEditing ? editEvent : handleEventCreation}
-                            nextBtnDisabled={
-                                (event.location === '' && !event.isRemote) ||
-                                event.startDate === '' ||
-                                event.endDate === ''
-                            }
-                        >
-                            <AdvancedEventInfo eventInfo={updateEvent} currentEvent={event} />
-                        </ProgressStep>
-                    </ProgressSteps>
-                </View>
-            )}
-            <CustomBottomTab />
+  return (
+    <View style={[styles.container]}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>
+          {isEditing ? "Editar evento" : "Crear evento"}
+        </Text>
+      </View>
+      {loading ? (
+        <View style={styles.loadingContainer}>
+          <LottieAnimation />
         </View>
-    )
-}
+      ) : (
+        <View style={styles.stepWrapper}>
+          <ProgressSteps
+            activeStepNumColor={colors.text}
+            completedProgressBarColor={colors.primary}
+            activeLabelColor={colors.text}
+            completedStepIconColor={colors.primary}
+            completedCheckColor={colors.primary}
+            activeStepIconBorderColor={colors.primary}
+            activeStepIconColor={colors.primary}
+            disabledStepIconColor={colors.disabled}
+            disabledStepNumColor={colors.white}
+            progressBarColor={colors.disabled}
+            labelFontFamily="SignikaRegular"
+          >
+            <ProgressStep
+              label="Categoría del evento"
+              nextBtnStyle={styles.nextBtnStyle}
+              nextBtnTextStyle={styles.nextBtnTextStyle}
+              nextBtnText="Siguiente"
+              nextBtnDisabled={category === ""}
+              scrollable={false}
+              viewProps={{ style: { flex: 1 } }}
+            >
+              <SelectCategory
+                categorySelected={setCategory}
+                activeCategory={category}
+              />
+            </ProgressStep>
+            <ProgressStep
+              label="Información básica"
+              nextBtnStyle={styles.nextBtnStyle}
+              nextBtnTextStyle={styles.nextBtnTextStyle}
+              nextBtnText="Siguiente"
+              previousBtnStyle={styles.previousBtnStyle}
+              previousBtnTextStyle={styles.previousBtnTextStyle}
+              previousBtnText="Atrás"
+              nextBtnDisabled={event.name === "" || event.description === ""}
+            >
+              <EventInfo eventInfo={updateEvent} currentEvent={event} />
+            </ProgressStep>
+            <ProgressStep
+              label="Información avanzada"
+              nextBtnStyle={styles.nextBtnStyle}
+              nextBtnTextStyle={styles.nextBtnTextStyle}
+              previousBtnStyle={styles.previousBtnStyle}
+              previousBtnTextStyle={styles.previousBtnTextStyle}
+              previousBtnText="Atrás"
+              finishBtnText={isEditing ? "Guardar Cambios" : "Crear Evento"}
+              onSubmit={isEditing ? editEvent : handleEventCreation}
+              nextBtnDisabled={
+                (event.location === "" && !event.isRemote) ||
+                event.startDate === "" ||
+                event.endDate === ""
+              }
+            >
+              <AdvancedEventInfo eventInfo={updateEvent} currentEvent={event} />
+            </ProgressStep>
+          </ProgressSteps>
+        </View>
+      )}
+      <CustomBottomTab />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: colors.background,
-        paddingTop: Platform.OS === 'android' ? 0 : 55,
-    },
-    titleContainer: {
-        alignItems: 'center'
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: colors.white,
-        fontFamily: 'SignikaBold',
-    },
-    stepWrapper: {
-        flex: 1,
-        paddingHorizontal: 10
-    },
-    nextBtnStyle: {
-        backgroundColor: colors.accent,
-        borderRadius: 4,
-        paddingHorizontal: 10
-    },
-    nextBtnTextStyle: {
-        color: 'black',
-        fontWeight: 'bold',
-        fontFamily: 'SignikaBold'
-    },
-    previousBtnStyle: {
-        backgroundColor: colors.secondary,
-        borderRadius: 4,
-        paddingHorizontal: 10
-    },
-    previousBtnTextStyle: {
-        color: colors.white,
-        fontWeight: 'bold',
-        fontFamily: 'SignikaBold'
-    },
-    loadingContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+    paddingTop: Platform.OS === "android" ? 0 : 55,
+  },
+  titleContainer: {
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: colors.white,
+    fontFamily: "SignikaBold",
+  },
+  stepWrapper: {
+    flex: 1,
+    paddingHorizontal: 10,
+  },
+  nextBtnStyle: {
+    backgroundColor: colors.accent,
+    borderRadius: 4,
+    paddingHorizontal: 10,
+  },
+  nextBtnTextStyle: {
+    color: "black",
+    fontWeight: "bold",
+    fontFamily: "SignikaBold",
+  },
+  previousBtnStyle: {
+    backgroundColor: colors.secondary,
+    borderRadius: 4,
+    paddingHorizontal: 10,
+  },
+  previousBtnTextStyle: {
+    color: colors.white,
+    fontWeight: "bold",
+    fontFamily: "SignikaBold",
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
-export default CreateEvent
+export default CreateEvent;
